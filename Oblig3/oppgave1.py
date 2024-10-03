74ebfc1097e51264f7c7aebeb3b815fa3a757394
NadiiaKuz <NadinK1603@gmail.com>
2024-10-03 13:34:28 +0200

    added oppgave1 til oblig3

---
+++ Oblig3/oppgave1.py
@@ -0,0 +1,17 @@
+student = {
+    "first name" : "Ola",
+    "last name" : "Nordmann",
+    "favourite course" : "Programmering 1"
+}
+print(student)
+print("--------------------------------------------------")
+
+print(f"Studentens fullstendige navn er {student["first name"]} {student["last name"]}")
+print("--------------------------------------------------")
+
+student["favourite course"] = "ITF10219 " + student["favourite course"]
+print(student)
+print("--------------------------------------------------")
+
+student["age"] = 30
+print(student)
