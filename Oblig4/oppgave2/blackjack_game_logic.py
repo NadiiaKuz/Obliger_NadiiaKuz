c0a7e6583127a58ea538e80be7cc90fd528cbdda
NadiiaKuz <NadinK1603@gmail.com>
2024-11-06 19:09:51 +0100

    refactor

--- Oblig4/oppgave2/blackjack_game_logic.py
+++ Oblig4/oppgave2/blackjack_game_logic.py
@@ -1,3 +1,21 @@
+from cards import Deck
+
+def get_new_deck():
+    deck = Deck()
+    deck.shuffle_deck()
+    return deck
+
+def make_bet(chips):
+    while True:
+        try:
+            bet_chips = int(input(f"You currently have {chips} chips. How many do you bet?: "))
+            if bet_chips <= 0 or bet_chips > chips:
+                print("Invalid bet. Please enter a valid amount: ")
+            else:
+                return bet_chips
+        except ValueError:
+            print("Invalid input!")
+
 # funksjon for å beregne summen av verdiene til kortene i hånden og endre verdien av esset hvis det trenges
 def calculate_score(hand):
     score = 0
@@ -13,10 +31,9 @@ def calculate_score(hand):
         ace_count -= 1
     return score
 
-def play_player (deck, player_hand):
+def get_hit_of_stand():
     while True:
-        print("----------------------------------------------------------------------------------")
-        try: #sjekker at input 1 eller 2
+        try:  # sjekker at input 1 eller 2
             hit_or_stand_choice = int(input("Do you wish to hit or stand? \n 1 - Hit \n 2 - Stand \n Answer: "))
             if hit_or_stand_choice not in [1, 2]:
                 print("Invalid choice. Please enter 1 for Hit or 2 for Stand.")
@@ -24,23 +41,7 @@ def play_player (deck, player_hand):
         except ValueError:
             print("Invalid input. Please enter a number (1 or 2).")
             continue
-
-        if hit_or_stand_choice == 1:
-            player_hand.append(deck.deal_card())
-
-            print(f"You have been dealt one card. Your hand now consists of the following cards:")
-            for card in player_hand:
-                print(f"- {card}")
-            score = calculate_score(player_hand)
-            print(f"The total value of your hand is now {score}")
-
-            if score > 21: # spilleren taper, spillet er over
-                print("----------------------------------------------------------------------------------")
-                print(f"Bust! You lose")
-                return False
-        else:
-            print("You chose to Stand")
-            return True
+        return hit_or_stand_choice
 
 def play_dealer (deck, dealer_hand):
     while calculate_score(dealer_hand) < 17:
@@ -49,17 +50,6 @@ def play_dealer (deck, dealer_hand):
     for card in dealer_hand:
         print(f"- {card}")
     return dealer_hand
-
-def get_winner (player_score, dealer_score, chips, bet_chips):
-    if (dealer_score > 21 or player_score > dealer_score):
-        print("You win!")
-        chips += bet_chips * 2
-    elif (dealer_score > player_score):
-        print("Dealer win!")
-    else:
-        print("No one win")
-        chips += bet_chips
-    return chips
 
 def play_again():
     while True:
--- Oblig4/oppgave2/play_game.py
+++ Oblig4/oppgave2/play_game.py
@@ -1,32 +1,19 @@
-from cards import Deck
 import blackjack_game_logic as b
 
-deck = Deck()
-deck.shuffle_deck()
+deck = b.get_new_deck()
 chips = 7
-
 while True:
-    if (len(deck.cards) < 15):
-        deck = Deck()
-        deck.shuffle_deck()
-        print("Deck reshuffled due to low card count.")
-
     if (chips == 0):
         print("Sorry! You do not have any chips to bet")
         break
 
-    while True:
-        try:
-            bet_chips = int(input(f"You currently have {chips} chips. How many do you bet?: "))
-            if bet_chips <= 0 or bet_chips > chips:
-                bet_chips = int(input("Invalid bet. Please enter a valid amount: "))
-            else:
-                break
-        except ValueError:
-            print("Invalid input!")
+    if (len(deck.cards) < 15):
+        deck = b.get_new_deck()
+        print("Deck reshuffled due to low card count.")
+
+    bet_chips = b.make_bet(chips)
     chips -= bet_chips
     print(f"You bet {bet_chips} chips. You have {chips} left after betting.")
-
 
     player_hand = [deck.deal_card(), deck.deal_card()]
     dealer_hand = [deck.deal_card(), deck.deal_card()]
@@ -35,31 +22,60 @@ while True:
     dealer_score = b.calculate_score(dealer_hand)
 
     print("----------------------------------------------------------------------------------")
-
     print(f"The cards have been dealt. You have a {player_hand[0]} and a {player_hand[1]}, with a total value of {player_score}")
     print(f"The dealers visible card is a {dealer_hand[0]}")
 
-    # Tester av Blackjack
+    # Sjekker av Blackjack
     if (player_score == 21):
         print("Blackjack! You win!")
         chips += bet_chips * 2
+
     else:
-        if (not b.play_player(deck, player_hand)): #
-            # Spilleren tapte på grunn av "bust"
-            print(f"You have {chips} chips")
+        while True:
             print("----------------------------------------------------------------------------------")
-        else:
+            hit_or_stand_choice = b.get_hit_of_stand()
+            overdraft = False
+
+            if hit_or_stand_choice == 1:
+                player_hand.append(deck.deal_card())
+
+                print(f"You have been dealt one card. Your hand now consists of the following cards:")
+                for card in player_hand:
+                    print(f"- {card}")
+                player_score = b.calculate_score(player_hand)
+                print(f"The total value of your hand is now {player_score}")
+
+                if (player_score > 21):
+                    print("Bust! You lose!")
+                    print(f"You have {chips} chips")
+                    overdraft = True
+                    break
+
+            else:
+                print("You chose to Stand")
+                break
+
+        if (not overdraft):
             # Hvis spilleren ikke har busted, spiller dealeren
-            b.play_dealer(deck, dealer_hand)
+            dealer_hand = b.play_dealer(deck, dealer_hand)
 
             player_score = b.calculate_score(player_hand)
             dealer_score = b.calculate_score(dealer_hand)
 
             print(f"The total value of the dealer hand is {dealer_score} compared to your hand's value {player_score}")
             print("----------------------------------------------------------------------------------")
-            chips = b.get_winner(player_score, dealer_score, chips, bet_chips)
-            print(f"You have {chips} chips now")
-            print("----------------------------------------------------------------------------------")
+
+            if (dealer_score > 21 or player_score > dealer_score):
+                print("You win!")
+                chips += bet_chips * 2
+            elif (dealer_score > player_score):
+                print("Dealer win!")
+            else:
+                print("No one win")
+                chips += bet_chips
+
+    print(f"You have {chips} chips now")
+    print("----------------------------------------------------------------------------------")
 
     if (not b.play_again()):
         break
